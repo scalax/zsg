4bd4d263a408db5d4983d96b48cba7a5b5c0dd0b
djx314 <djx314@sina.cn>
2018-05-16 04:41:58 +0800

    More simple.

--- src/main/scala/net/scalax/asuna/shape/DataShapeValue.scala
+++ src/main/scala/net/scalax/asuna/shape/DataShapeValue.scala
@@ -2,38 +2,25 @@ package net.scalax.asuna.shape
 
 import net.scalax.asuna.core._
 
-trait DataShapeValue[E, U, C, T] {
+trait DataShapeValue[E, U, T] {
   val rep: E
-  val shape: DataShape[E, U, C, T]
+  val shape: DataShape[E, U, E, T]
 }
 
 object DataShapeValue {
 
-  implicit def dataShapeValueShape[E, U, C, T]: DataShape[DataShapeValue[E, U, C, T], U, DataShapeValue[C, U, C, T], T] = {
+  implicit def dataShapeValueShape[E, U, T, R <: DataShapeValue[E, U, T]]: DataShape[R, U, R, T] = {
 
-    new DataShape[DataShapeValue[E, U, C, T], U, DataShapeValue[C, U, C, T], T] {
+    new DataShape[R, U, R, T] {
       self =>
 
-      override def packed: DataShape[DataShapeValue[C, U, C, T], U, DataShapeValue[C, U, C, T], T] = new DataShape[DataShapeValue[C, U, C, T], U, DataShapeValue[C, U, C, T], T] {
-        subSelf =>
+      override def packed: DataShape[R, U, R, T] = self
 
-        override def packed: DataShape[DataShapeValue[C, U, C, T], U, DataShapeValue[C, U, C, T], T] = subSelf
+      override def wrapRep(base: R): R = base
 
-        override def wrapRep(base: DataShapeValue[C, U, C, T]): DataShapeValue[C, U, C, T] = base
+      override def toLawRep(base: R): DataRepGroup[T] = base.shape.toLawRep(base.shape.wrapRep(base.rep))
 
-        override def toLawRep(base: DataShapeValue[C, U, C, T]): DataRepGroup[T] = self.toLawRep(base)
-
-        override def takeData(oldData: DataGroup, rep: DataShapeValue[C, U, C, T]): SplitData[U] = self.takeData(oldData, rep)
-      }
-
-      override def wrapRep(base: DataShapeValue[E, U, C, T]): DataShapeValue[C, U, C, T] = new DataShapeValue[C, U, C, T] {
-        override val rep = base.shape.wrapRep(base.rep)
-        override val shape = base.shape.packed
-      }
-
-      override def toLawRep(base: DataShapeValue[C, U, C, T]): DataRepGroup[T] = base.shape.toLawRep(base.shape.wrapRep(base.rep))
-
-      override def takeData(oldData: DataGroup, rep: DataShapeValue[C, U, C, T]): SplitData[U] = rep.shape.takeData(oldData, rep.rep)
+      override def takeData(oldData: DataGroup, rep: R): SplitData[U] = rep.shape.takeData(oldData, rep.rep)
 
     }
 
