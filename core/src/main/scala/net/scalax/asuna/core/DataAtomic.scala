3e0b38620dc067592dace150df6fb66e21f38ef8
djx314 <djx314@sina.cn>
2018-06-08 04:57:31 +0800

    Remove unused code.

--- core/src/main/scala/net/scalax/asuna/core/DataAtomic.scala
+++ core/src/main/scala/net/scalax/asuna/core/DataAtomic.scala
@@ -1,12 +1,9 @@
 package net.scalax.asuna.core
 
-import shapeless._
-import tag._
-
 trait AtomicColumn[Data, CommonCol] {
   def common: CommonCol
 }
-
+/*
 trait OutputTag
 trait InputTag
 trait IOTag
@@ -21,4 +18,4 @@ object AtomicColumn {
   def tagSub[T <: AtomicColumn[_, _]](ac: T): T @@ SubTag = tag[SubTag](ac)
   def tagOutputSub[T <: AtomicColumn[_, _]](ac: T): T @@ OutputSubTag = tag[OutputSubTag](ac)
 
-}
+}*/ 
