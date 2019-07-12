b2a08dfc4c8dbc96dc548433f76db74fac90e391
djx314 <djx314@sina.cn>
2019-07-12 10:00:51 +0800

    manyi

--- src/test/scala/jsonencoder/compare/manying/Runner.scala
+++ src/test/scala/jsonencoder/compare/manying/Runner.scala
@@ -1,16 +1,22 @@
-import scala.language.higherKinds
-
 object ThirdTest extends App {
 
   class HListM[I <: HList, B <: Chaju](val hlist: I, val tran: B) {
+    type Add[Item] = HListM[B#M[I, Item], B#Next]
     def add[Item](item: Item): HListM[B#M[I, Item], B#Next] = new HListM(tran.tran(hlist, item), tran.next)
     override def toString: String = hlist.toString
   }
 
-  object HListMImpl extends HListM(HNil, Manyi.add.add.add.add)
+  class ZeroHListM extends HListM(HNil, Manyi.add.add.add.add)
+  object ZeroHListM extends ZeroHListM
 
-  val item = HListMImpl.add(1).add("个").add("喵喵喵").add("满").add("自").add("溢").add("的").add("HList")
+  val item: ZeroHListM#Add[Int]#Add[String]#Add[String]#Add[String]#Add[String]#Add[String]#Add[String]#Add[String] =
+    ZeroHListM.add(1).add("个").add("喵喵喵").add("满").add("自").add("溢").add("的").add("HList")
   println(item.toString)
   val b: String :: String :: String :: Int :: HNil = item.hlist //检查类型
+
+  //脱离实体的运算
+  val ii: ZeroHListM#Add[Int]#Add[String]#Add[String]#Add[String]#Add[String]#Add[String]#Add[String]#Add[String] = throw new Exception("abc")
+  val iiii: ZeroHListM#Add[Int]#Add[String]#Add[String]#Add[String] = ii
+  val iiiiii: String :: String :: String :: Int :: HNil = iiii.hlist
 
 }
