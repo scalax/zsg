2f9828afa90c35f6be58b04221ad045327c155cf
djx314 <djx314@sina.cn>
2019-07-15 17:43:28 +0800

    m

--- src/test/scala/jsonencoder/compare/change/ZeroHList.scala
+++ src/test/scala/jsonencoder/compare/change/ZeroHList.scala
@@ -8,17 +8,17 @@ object Runner extends App {
   class Init[YY <: HListYuan](yueyuan: YY) extends YQImpl[YY, YuequeZero, 新月, YuanToQue](yueyuan, YuequeZero, new 新月, YuanToQue)
 
   type Yue = Init[YueyuanZero#Add[满月]#Add[渐盈]#Add[上弦]]
-  val a0: 新月 = (throw new Exception("ii")): Yue#Current
-  val a1: 上弦 = (throw new Exception("ii")): Yue#Next#Current
-  val a2: 渐盈 = (throw new Exception("ii")): Yue#Next#Next#Current
-  val a3: 满月 = (throw new Exception("ii")): Yue#Next#Next#Next#Current
+  val a1: 新月 = (throw new Exception("ii")): Yue#Current
+  val a2: 上弦 = (throw new Exception("ii")): Yue#Next#Current
+  val a3: 渐盈 = (throw new Exception("ii")): Yue#Next#Next#Current
+  val a4: 满月 = (throw new Exception("ii")): Yue#Next#Next#Next#Current
 
-  val a4: 渐盈 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Current
-  val a5: 上弦 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Current
-  val a6: 新月 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Current
+  val a5: 渐盈 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Current
+  val a6: 上弦 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Current
+  val a7: 新月 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Current
 
-  val a7: 上弦 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Current
-  val a8: 渐盈 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Next#Current
-  val a9: 满月 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
+  val a8: 上弦  = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Current
+  val a9: 渐盈  = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Next#Current
+  val a10: 满月 = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
 
 }
