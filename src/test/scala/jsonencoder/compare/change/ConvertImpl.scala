5ac94702774ae21f7eb5d076179b4649c91b5b33
djx314 <djx314@sina.cn>
2019-07-15 16:39:47 +0800

    some fix.

--- src/test/scala/jsonencoder/compare/change/ConvertImpl.scala
+++ src/test/scala/jsonencoder/compare/change/ConvertImpl.scala
@@ -1,16 +1,16 @@
 class YuanToQue extends Convert {
-  override type M[I <: HListYQ] = YQImpl[I#Yueyuan#Tail, I#Yueque#Add[I#Current], I#Yueyuan#Head, I#Yueyuan#C]
-  override def convert[I <: HListYQ](i: I): YQImpl[I#Yueyuan#Tail, I#Yueque#Add[I#Current], I#Yueyuan#Head, I#Yueyuan#C] = {
-    new YQImpl(i.yueyuan.tail, i.yueque.add(i.current), i.yueyuan.head, i.yueyuan.c)
+  override type M[I <: HListYQ] = YQImpl[I#Yueyuan#Tail, I#Yueque#Add[I#Current], I#Yueyuan#Head, I#Yueyuan#Tail#C]
+  override def convert[I <: HListYQ](i: I): YQImpl[I#Yueyuan#Tail, I#Yueque#Add[I#Current], I#Yueyuan#Head, I#Yueyuan#Tail#C] = {
+    new YQImpl(i.yueyuan.tail, i.yueque.add(i.current), i.yueyuan.head, i.yueyuan.tail.c)
   }
 }
 
 object YuanToQue extends YuanToQue
 
 class QueToYuan extends Convert {
-  override type M[I <: HListYQ] = YQImpl[I#Yueyuan#Add[I#Current], I#Yueque#Tail, I#Yueque#Head, I#Yueque#C]
-  override def convert[I <: HListYQ](i: I): YQImpl[I#Yueyuan#Add[I#Current], I#Yueque#Tail, I#Yueque#Head, I#Yueque#C] = {
-    new YQImpl(i.yueyuan.add(i.current), i.yueque.tail, i.yueque.head, i.yueque.c)
+  override type M[I <: HListYQ] = YQImpl[I#Yueyuan#Add[I#Current], I#Yueque#Tail, I#Yueque#Head, I#Yueque#Tail#C]
+  override def convert[I <: HListYQ](i: I): YQImpl[I#Yueyuan#Add[I#Current], I#Yueque#Tail, I#Yueque#Head, I#Yueque#Tail#C] = {
+    new YQImpl(i.yueyuan.add(i.current), i.yueque.tail, i.yueque.head, i.yueque.tail.c)
   }
 }
 
--- src/test/scala/jsonencoder/compare/change/ZeroHList.scala
+++ src/test/scala/jsonencoder/compare/change/ZeroHList.scala
@@ -1,46 +1,45 @@
 object Runner extends App {
 
-  class Init[YY <: HListYuan](yueyuan: YY) extends YQImpl(yueyuan, `月缺`, 1, QueToYuan)
+  class Init[YY <: HListYuan](yueyuan: YY) extends YQImpl[YY, `月缺`, Int, YuanToQue](yueyuan, `月缺`, 1, YuanToQue)
 
   type Yue = Init[`月圆`#Add[Int]#Add[Int]#Add[String]]
   val a0: Int    = (throw new Exception("ii")): Yue#Current
-  val a1: `月缺`   = (throw new Exception("ii")): Yue#Next#Current
+  val a1: String = (throw new Exception("ii")): Yue#Next#Current
   val a2: Int    = (throw new Exception("ii")): Yue#Next#Next#Current
-  val a3: String = (throw new Exception("ii")): Yue#Next#Next#Next#Current
-  val aa: Yue#Next#Next = ""
+  val a3: Int    = (throw new Exception("ii")): Yue#Next#Next#Next#Current
   val a4: Int    = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Current
-  val a5: Int    = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Current
-  val a6: `月圆`   = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Current
-  val a7: Int    = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Current
+  val a5: String = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Current
+  val a6: Int    = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Current
+  val a7: String = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Current
   val a8: Int    = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Next#Current
-  val a9: String = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
+  val a9: Int    = (throw new Exception("ii")): Yue#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
 
   type Next10 = Yue#Next#Next#Next#Next#Next#Next#Next#Next#Next#Next
   val a10: Int    = (throw new Exception("ii")): Next10#Current
-  val a11: `月缺`   = (throw new Exception("ii")): Next10#Next#Current
-  val a12: `月缺`   = (throw new Exception("ii")): Next10#Next#Next#Current
-  val a13: `月缺`   = (throw new Exception("ii")): Next10#Next#Next#Next#Current
+  val a11: String = (throw new Exception("ii")): Next10#Next#Current
+  val a12: Int    = (throw new Exception("ii")): Next10#Next#Next#Current
+  val a13: String = (throw new Exception("ii")): Next10#Next#Next#Next#Current
   val a14: Int    = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Current
-  val a15: String = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Current
-  val a16: Int    = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Current
-  val a17: Int    = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Next#Current
-  val a18: `月圆`   = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Next#Next#Current
-  val a19: `月圆`   = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
+  val a15: Int    = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Current
+  // val a16: Int    = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Current
+  // val a17: Int    = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Next#Current
+  // val a18: `月圆`   = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Next#Next#Current
+  // val a19: `月圆`   = (throw new Exception("ii")): Next10#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
 
-  type Next20 = Next10#Next#Next#Next#Next#Next#Next#Next#Next#Next#Next
-  val a20: `月圆`   = (throw new Exception("ii")): Next20#Current
-  val a21: Int    = (throw new Exception("ii")): Next20#Next#Current
-  val a22: Int    = (throw new Exception("ii")): Next20#Next#Next#Current
-  val a23: String = (throw new Exception("ii")): Next20#Next#Next#Next#Current
-  val a24: Int    = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Current
-  val a25: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Current
-  val a26: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Current
-  val a27: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Next#Current
-  val a28: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Next#Next#Current
-  val a29: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
+  // type Next20 = Next10#Next#Next#Next#Next#Next#Next#Next#Next#Next#Next
+  // val a20: `月圆`   = (throw new Exception("ii")): Next20#Current
+  // val a21: Int    = (throw new Exception("ii")): Next20#Next#Current
+  // val a22: Int    = (throw new Exception("ii")): Next20#Next#Next#Current
+  // val a23: String = (throw new Exception("ii")): Next20#Next#Next#Next#Current
+  // val a24: Int    = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Current
+  // val a25: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Current
+  // val a26: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Current
+  // val a27: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Next#Current
+  // val a28: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Next#Next#Current
+  // val a29: `月缺`   = (throw new Exception("ii")): Next20#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current
 
-  type Next30 = Next20#Next#Next#Next#Next#Next#Next#Next#Next#Next#Next
+  // type Next30 = Next20#Next#Next#Next#Next#Next#Next#Next#Next#Next#Next
 
-  val a30: Int = (throw new Exception("ii")): Next30#Current
+  // val a30: Int = (throw new Exception("ii")): Next30#Current
 
 }
