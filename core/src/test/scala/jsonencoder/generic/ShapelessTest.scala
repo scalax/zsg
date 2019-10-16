a0a748e9f0aa635348dd059d405e10934e3b4278
djx314 <djx314@sina.cn>
2019-10-16 22:17:34 +0800

    Add test case

--- core/src/test/scala/jsonencoder/generic/ShapelessTest.scala
+++ core/src/test/scala/jsonencoder/generic/ShapelessTest.scala
@@ -7,11 +7,60 @@ import shapeless.labelled.FieldType
 
 object ShapelessTest {
 
-  object EncodeImplicit {
+  object EncodeImplicit extends EncodeImplicit3 {
 
     implicit val encodeHNil: Encoder.AsObject[HNil] = Encoder.AsObject.instance((_: HNil) => JsonObject.empty)
 
-    implicit def encodeHCons[K <: Symbol, H, T <: HList](
+    /*implicit def encodeHCons[K <: Symbol, H, T <: HList](
+      implicit
+      key: Witness.Aux[K],
+      encodeH: LazyImplicit[Encoder[H]],
+      encodeT: Encoder.AsObject[T]
+    ): Encoder.AsObject[FieldType[K, H] :: T] = Encoder.AsObject.instance {
+      case h :: t => ((key.value.name, encodeH.value(h))) +: encodeT.encodeObject(t)
+    }*/
+
+    def encodeGeneric[A, R](implicit gen: LabelledGeneric.Aux[A, R], encodeR: Encoder.AsObject[R]): Encoder.AsObject[A] =
+      Encoder.AsObject.instance(a => encodeR.encodeObject(gen.to(a)))
+
+  }
+
+  trait EncodeImplicit3 extends EncodeImplicit2 {
+
+    implicit def encodeHCons3[K1 <: Symbol, H1, K2 <: Symbol, H2, K3 <: Symbol, H3, T <: HList](
+      implicit
+      key1: Witness.Aux[K1],
+      encodeH1: LazyImplicit[Encoder[H1]],
+      key2: Witness.Aux[K2],
+      encodeH2: LazyImplicit[Encoder[H2]],
+      key3: Witness.Aux[K3],
+      encodeH3: LazyImplicit[Encoder[H3]],
+      encodeT: Encoder.AsObject[T]
+    ): Encoder.AsObject[FieldType[K1, H1] :: FieldType[K2, H2] :: FieldType[K3, H3] :: T] = Encoder.AsObject.instance {
+      case h1 :: h2 :: h3 :: t =>
+        ((key3.value.name, encodeH3.value(h3))) +: ((key2.value.name, encodeH2.value(h2))) +: ((key1.value.name, encodeH1.value(h1))) +: encodeT.encodeObject(t)
+    }
+
+  }
+
+  trait EncodeImplicit2 extends EncodeImplicit1 {
+
+    implicit def encodeHCons2[K1 <: Symbol, H1, K2 <: Symbol, H2, T <: HList](
+      implicit
+      key1: Witness.Aux[K1],
+      encodeH1: LazyImplicit[Encoder[H1]],
+      key2: Witness.Aux[K2],
+      encodeH2: LazyImplicit[Encoder[H2]],
+      encodeT: Encoder.AsObject[T]
+    ): Encoder.AsObject[FieldType[K1, H1] :: FieldType[K2, H2] :: T] = Encoder.AsObject.instance {
+      case h1 :: h2 :: t => ((key2.value.name, encodeH2.value(h2))) +: ((key1.value.name, encodeH1.value(h1))) +: encodeT.encodeObject(t)
+    }
+
+  }
+
+  trait EncodeImplicit1 {
+
+    implicit def encodeHCons1[K <: Symbol, H, T <: HList](
       implicit
       key: Witness.Aux[K],
       encodeH: LazyImplicit[Encoder[H]],
@@ -19,9 +68,6 @@ object ShapelessTest {
     ): Encoder.AsObject[FieldType[K, H] :: T] = Encoder.AsObject.instance {
       case h :: t => ((key.value.name, encodeH.value(h))) +: encodeT.encodeObject(t)
     }
-
-    def encodeGeneric[A, R](implicit gen: LabelledGeneric.Aux[A, R], encodeR: Encoder.AsObject[R]): Encoder.AsObject[A] =
-      Encoder.AsObject.instance(a => encodeR.encodeObject(gen.to(a)))
 
   }
 
