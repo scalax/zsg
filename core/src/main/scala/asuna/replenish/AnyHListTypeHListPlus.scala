947f830b5f9b9702c3fd7fa5c62134418289d980
djx314 <djx314@sina.cn>
2019-10-23 15:47:09 +0800

    Clear tpye define

--- core/src/main/scala/asuna/replenish/AnyHListTypeHListPlus.scala
+++ core/src/main/scala/asuna/replenish/AnyHListTypeHListPlus.scala
@@ -6,17 +6,17 @@ trait AnyHListTypeHListPlus {
   self =>
 
   val plus: Plus[AnyHListTypeHList, AnyTypeHList, AnyHListTypeHList] = new Plus[AnyHListTypeHList, AnyTypeHList, AnyHListTypeHList] {
-    override def plus(p: HList, item: Any): HList                                   = item :: p
-    override def takeHead(t: HList): HList                                          = t.tail
-    override def takeTail(t: HList): Any                                            = t.head
-    override lazy val sub: Plus[AnyHListTypeHList, AnyTypeHList, AnyHListTypeHList] = self.plus
+    override def plus(p: AnyHListTypeHList#H, item: AnyTypeHList#H): AnyHListTypeHList#H  = item :: p
+    override def takeHead(t: AnyHListTypeHList#H): AnyHListTypeHList#H                    = t.tail
+    override def takeTail(t: AnyHListTypeHList#H): AnyTypeHList#H                         = t.head
+    override lazy val sub: Plus[AnyHListTypeHList#T, AnyTypeHList#T, AnyHListTypeHList#T] = self.plus
   }
 
   val plus0To1: Plus[TupleTypeHList0, AnyTypeHList, AnyHListTypeHList] = new Plus[TupleTypeHList0, AnyTypeHList, AnyHListTypeHList] {
-    override def plus(p: AsunaTuple0, item: Any): HList                           = item :: HNil
-    override def takeHead(t: HList): AsunaTuple0                                  = AsunaTuple0
-    override def takeTail(t: HList): Any                                          = t.head
-    override lazy val sub: Plus[TupleTypeHList0, AnyTypeHList, AnyHListTypeHList] = self.plus0To1
+    override def plus(p: TupleTypeHList0#H, item: AnyTypeHList#H): AnyHListTypeHList#H  = item :: HNil
+    override def takeHead(t: AnyHListTypeHList#H): TupleTypeHList0#H                    = AsunaTuple0
+    override def takeTail(t: AnyHListTypeHList#H): AnyTypeHList#H                       = t.head
+    override lazy val sub: Plus[TupleTypeHList0#T, AnyTypeHList#T, AnyHListTypeHList#T] = self.plus0To1
   }
 
   @inline def hlistPlus1[T1 <: TypeHList]: Plus[TupleTypeHList0, T1, HListTypeHList1[T1]] = plus0To1.asInstanceOf[Plus[TupleTypeHList0, T1, HListTypeHList1[T1]]]
