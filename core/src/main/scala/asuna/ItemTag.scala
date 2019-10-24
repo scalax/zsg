784ca65bac562ff77e43f9a49a7e57d5495c8cc2
djx314 <djx314@sina.cn>
2019-10-24 15:29:15 +0800

    Remove code.

--- core/src/main/scala/asuna/ItemTag.scala
+++ core/src/main/scala/asuna/ItemTag.scala
@@ -7,13 +7,7 @@ package asuna {
     type M[M <: Message] <: TupleTag
   }
 
-  class EndItemTag extends TupleTag
-
   trait Message
-
-  /*trait ContextContent[T] {
-    def withContext[K <: KindContext](c: Context[K]): T
-  }*/
 
   package impl {
     case class :-<>-:[M1 <: Message, M2 <: Message](m1: M1, m2: M2) extends Message
