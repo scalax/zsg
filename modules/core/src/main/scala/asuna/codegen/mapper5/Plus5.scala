3a4087c4f97c74e06465a8a341428f986d13b167
djx314 <djx314@sina.cn>
2019-11-06 09:15:04 +0800

    Optimize performance.

--- modules/core/src/main/scala/asuna/codegen/mapper5/Plus5.scala
+++ modules/core/src/main/scala/asuna/codegen/mapper5/Plus5.scala
@@ -16,8 +16,8 @@ trait Plus5[X1, X2, X3, X4, X5, Y1, Y2, Y3, Y4, Y5, Z1, Z2, Z3, Z4, Z5] {
   def takeTail5(z: Z5): Y5
   def plus5(x: X5, y: Y5): Z5
 }
-object Plus5 {
-  def plusWithTypeParameter0[X1_C1, X2_C1, X3_C1, X4_C1, X5_C1, Y1, Y2, Y3, Y4, Y5]: Plus5[
+final object Plus5 {
+  final def cachePlusWithTypeParameter0[X1_C1, X2_C1, X3_C1, X4_C1, X5_C1, Y1, Y2, Y3, Y4, Y5]: Plus5[
     AsunaTuple0,
     AsunaTuple0,
     AsunaTuple0,
@@ -67,7 +67,58 @@ object Plus5 {
       override def takeTail5(z: AsunaTuple1[Y5]): Y5             = z.head
       override def plus5(x: AsunaTuple0, y: Y5): AsunaTuple1[Y5] = x.::(y)
     }
-  def plusWithTypeParameter1[X1_C1, X2_C1, X3_C1, X4_C1, X5_C1, Y1, Y2, Y3, Y4, Y5]: Plus5[
+  final val valPlusWithTypeParameter0: Plus5[
+    AsunaTuple0,
+    AsunaTuple0,
+    AsunaTuple0,
+    AsunaTuple0,
+    AsunaTuple0,
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple1[Any],
+    AsunaTuple1[Any],
+    AsunaTuple1[Any],
+    AsunaTuple1[Any],
+    AsunaTuple1[Any]
+  ] = cachePlusWithTypeParameter0
+  final def plusWithTypeParameter0[X1_C1, X2_C1, X3_C1, X4_C1, X5_C1, Y1, Y2, Y3, Y4, Y5]: Plus5[
+    AsunaTuple0,
+    AsunaTuple0,
+    AsunaTuple0,
+    AsunaTuple0,
+    AsunaTuple0,
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5,
+    AsunaTuple1[Y1],
+    AsunaTuple1[Y2],
+    AsunaTuple1[Y3],
+    AsunaTuple1[Y4],
+    AsunaTuple1[Y5]
+  ] =
+    valPlusWithTypeParameter0.asInstanceOf[Plus5[
+      AsunaTuple0,
+      AsunaTuple0,
+      AsunaTuple0,
+      AsunaTuple0,
+      AsunaTuple0,
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple1[Y1],
+      AsunaTuple1[Y2],
+      AsunaTuple1[Y3],
+      AsunaTuple1[Y4],
+      AsunaTuple1[Y5]
+    ]]
+  final def cachePlusWithTypeParameter1[X1_C1, X2_C1, X3_C1, X4_C1, X5_C1, Y1, Y2, Y3, Y4, Y5]: Plus5[
     AsunaTuple1[X1_C1],
     AsunaTuple1[X2_C1],
     AsunaTuple1[X3_C1],
@@ -117,7 +168,58 @@ object Plus5 {
       override def takeTail5(z: AsunaTuple2[Y5, X5_C1]): Y5                    = z.head
       override def plus5(x: AsunaTuple1[X5_C1], y: Y5): AsunaTuple2[Y5, X5_C1] = x.::(y)
     }
-  def plusWithTypeParameter2[X1_C1, X1_C2, X2_C1, X2_C2, X3_C1, X3_C2, X4_C1, X4_C2, X5_C1, X5_C2, Y1, Y2, Y3, Y4, Y5]: Plus5[
+  final val valPlusWithTypeParameter1: Plus5[
+    AsunaTuple1[Any],
+    AsunaTuple1[Any],
+    AsunaTuple1[Any],
+    AsunaTuple1[Any],
+    AsunaTuple1[Any],
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any]
+  ] = cachePlusWithTypeParameter1
+  final def plusWithTypeParameter1[X1_C1, X2_C1, X3_C1, X4_C1, X5_C1, Y1, Y2, Y3, Y4, Y5]: Plus5[
+    AsunaTuple1[X1_C1],
+    AsunaTuple1[X2_C1],
+    AsunaTuple1[X3_C1],
+    AsunaTuple1[X4_C1],
+    AsunaTuple1[X5_C1],
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5,
+    AsunaTuple2[Y1, X1_C1],
+    AsunaTuple2[Y2, X2_C1],
+    AsunaTuple2[Y3, X3_C1],
+    AsunaTuple2[Y4, X4_C1],
+    AsunaTuple2[Y5, X5_C1]
+  ] =
+    valPlusWithTypeParameter1.asInstanceOf[Plus5[
+      AsunaTuple1[X1_C1],
+      AsunaTuple1[X2_C1],
+      AsunaTuple1[X3_C1],
+      AsunaTuple1[X4_C1],
+      AsunaTuple1[X5_C1],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple2[Y1, X1_C1],
+      AsunaTuple2[Y2, X2_C1],
+      AsunaTuple2[Y3, X3_C1],
+      AsunaTuple2[Y4, X4_C1],
+      AsunaTuple2[Y5, X5_C1]
+    ]]
+  final def cachePlusWithTypeParameter2[X1_C1, X1_C2, X2_C1, X2_C2, X3_C1, X3_C2, X4_C1, X4_C2, X5_C1, X5_C2, Y1, Y2, Y3, Y4, Y5]: Plus5[
     AsunaTuple2[X1_C1, X1_C2],
     AsunaTuple2[X2_C1, X2_C2],
     AsunaTuple2[X3_C1, X3_C2],
@@ -167,23 +269,75 @@ object Plus5 {
       override def takeTail5(z: AsunaTuple3[Y5, X5_C1, X5_C2]): Y5                           = z.head
       override def plus5(x: AsunaTuple2[X5_C1, X5_C2], y: Y5): AsunaTuple3[Y5, X5_C1, X5_C2] = x.::(y)
     }
-  def plusWithTypeParameter3[X1_C1, X1_C2, X1_C3, X2_C1, X2_C2, X2_C3, X3_C1, X3_C2, X3_C3, X4_C1, X4_C2, X4_C3, X5_C1, X5_C2, X5_C3, Y1, Y2, Y3, Y4, Y5]: Plus5[
-    AsunaTuple3[X1_C1, X1_C2, X1_C3],
-    AsunaTuple3[X2_C1, X2_C2, X2_C3],
-    AsunaTuple3[X3_C1, X3_C2, X3_C3],
-    AsunaTuple3[X4_C1, X4_C2, X4_C3],
-    AsunaTuple3[X5_C1, X5_C2, X5_C3],
+  final val valPlusWithTypeParameter2: Plus5[
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any],
+    AsunaTuple2[Any, Any],
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any]
+  ] = cachePlusWithTypeParameter2
+  final def plusWithTypeParameter2[X1_C1, X1_C2, X2_C1, X2_C2, X3_C1, X3_C2, X4_C1, X4_C2, X5_C1, X5_C2, Y1, Y2, Y3, Y4, Y5]: Plus5[
+    AsunaTuple2[X1_C1, X1_C2],
+    AsunaTuple2[X2_C1, X2_C2],
+    AsunaTuple2[X3_C1, X3_C2],
+    AsunaTuple2[X4_C1, X4_C2],
+    AsunaTuple2[X5_C1, X5_C2],
     Y1,
     Y2,
     Y3,
     Y4,
     Y5,
-    AsunaTuple4[Y1, X1_C1, X1_C2, X1_C3],
-    AsunaTuple4[Y2, X2_C1, X2_C2, X2_C3],
-    AsunaTuple4[Y3, X3_C1, X3_C2, X3_C3],
-    AsunaTuple4[Y4, X4_C1, X4_C2, X4_C3],
-    AsunaTuple4[Y5, X5_C1, X5_C2, X5_C3]
+    AsunaTuple3[Y1, X1_C1, X1_C2],
+    AsunaTuple3[Y2, X2_C1, X2_C2],
+    AsunaTuple3[Y3, X3_C1, X3_C2],
+    AsunaTuple3[Y4, X4_C1, X4_C2],
+    AsunaTuple3[Y5, X5_C1, X5_C2]
   ] =
+    valPlusWithTypeParameter2.asInstanceOf[Plus5[
+      AsunaTuple2[X1_C1, X1_C2],
+      AsunaTuple2[X2_C1, X2_C2],
+      AsunaTuple2[X3_C1, X3_C2],
+      AsunaTuple2[X4_C1, X4_C2],
+      AsunaTuple2[X5_C1, X5_C2],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple3[Y1, X1_C1, X1_C2],
+      AsunaTuple3[Y2, X2_C1, X2_C2],
+      AsunaTuple3[Y3, X3_C1, X3_C2],
+      AsunaTuple3[Y4, X4_C1, X4_C2],
+      AsunaTuple3[Y5, X5_C1, X5_C2]
+    ]]
+  final def cachePlusWithTypeParameter3[X1_C1, X1_C2, X1_C3, X2_C1, X2_C2, X2_C3, X3_C1, X3_C2, X3_C3, X4_C1, X4_C2, X4_C3, X5_C1, X5_C2, X5_C3, Y1, Y2, Y3, Y4, Y5]
+    : Plus5[
+      AsunaTuple3[X1_C1, X1_C2, X1_C3],
+      AsunaTuple3[X2_C1, X2_C2, X2_C3],
+      AsunaTuple3[X3_C1, X3_C2, X3_C3],
+      AsunaTuple3[X4_C1, X4_C2, X4_C3],
+      AsunaTuple3[X5_C1, X5_C2, X5_C3],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple4[Y1, X1_C1, X1_C2, X1_C3],
+      AsunaTuple4[Y2, X2_C1, X2_C2, X2_C3],
+      AsunaTuple4[Y3, X3_C1, X3_C2, X3_C3],
+      AsunaTuple4[Y4, X4_C1, X4_C2, X4_C3],
+      AsunaTuple4[Y5, X5_C1, X5_C2, X5_C3]
+    ] =
     new Plus5[
       AsunaTuple3[X1_C1, X1_C2, X1_C3],
       AsunaTuple3[X2_C1, X2_C2, X2_C3],
@@ -217,7 +371,58 @@ object Plus5 {
       override def takeTail5(z: AsunaTuple4[Y5, X5_C1, X5_C2, X5_C3]): Y5                                  = z.head
       override def plus5(x: AsunaTuple3[X5_C1, X5_C2, X5_C3], y: Y5): AsunaTuple4[Y5, X5_C1, X5_C2, X5_C3] = x.::(y)
     }
-  def plusWithTypeParameter4[
+  final val valPlusWithTypeParameter3: Plus5[
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any],
+    AsunaTuple3[Any, Any, Any],
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any]
+  ] = cachePlusWithTypeParameter3
+  final def plusWithTypeParameter3[X1_C1, X1_C2, X1_C3, X2_C1, X2_C2, X2_C3, X3_C1, X3_C2, X3_C3, X4_C1, X4_C2, X4_C3, X5_C1, X5_C2, X5_C3, Y1, Y2, Y3, Y4, Y5]: Plus5[
+    AsunaTuple3[X1_C1, X1_C2, X1_C3],
+    AsunaTuple3[X2_C1, X2_C2, X2_C3],
+    AsunaTuple3[X3_C1, X3_C2, X3_C3],
+    AsunaTuple3[X4_C1, X4_C2, X4_C3],
+    AsunaTuple3[X5_C1, X5_C2, X5_C3],
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5,
+    AsunaTuple4[Y1, X1_C1, X1_C2, X1_C3],
+    AsunaTuple4[Y2, X2_C1, X2_C2, X2_C3],
+    AsunaTuple4[Y3, X3_C1, X3_C2, X3_C3],
+    AsunaTuple4[Y4, X4_C1, X4_C2, X4_C3],
+    AsunaTuple4[Y5, X5_C1, X5_C2, X5_C3]
+  ] =
+    valPlusWithTypeParameter3.asInstanceOf[Plus5[
+      AsunaTuple3[X1_C1, X1_C2, X1_C3],
+      AsunaTuple3[X2_C1, X2_C2, X2_C3],
+      AsunaTuple3[X3_C1, X3_C2, X3_C3],
+      AsunaTuple3[X4_C1, X4_C2, X4_C3],
+      AsunaTuple3[X5_C1, X5_C2, X5_C3],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple4[Y1, X1_C1, X1_C2, X1_C3],
+      AsunaTuple4[Y2, X2_C1, X2_C2, X2_C3],
+      AsunaTuple4[Y3, X3_C1, X3_C2, X3_C3],
+      AsunaTuple4[Y4, X4_C1, X4_C2, X4_C3],
+      AsunaTuple4[Y5, X5_C1, X5_C2, X5_C3]
+    ]]
+  final def cachePlusWithTypeParameter4[
     X1_C1,
     X1_C2,
     X1_C3,
@@ -293,7 +498,84 @@ object Plus5 {
       override def takeTail5(z: AsunaTuple5[Y5, X5_C1, X5_C2, X5_C3, X5_C4]): Y5                                         = z.head
       override def plus5(x: AsunaTuple4[X5_C1, X5_C2, X5_C3, X5_C4], y: Y5): AsunaTuple5[Y5, X5_C1, X5_C2, X5_C3, X5_C4] = x.::(y)
     }
-  def plusWithTypeParameter5[
+  final val valPlusWithTypeParameter4: Plus5[
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any],
+    AsunaTuple4[Any, Any, Any, Any],
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any]
+  ] = cachePlusWithTypeParameter4
+  final def plusWithTypeParameter4[
+    X1_C1,
+    X1_C2,
+    X1_C3,
+    X1_C4,
+    X2_C1,
+    X2_C2,
+    X2_C3,
+    X2_C4,
+    X3_C1,
+    X3_C2,
+    X3_C3,
+    X3_C4,
+    X4_C1,
+    X4_C2,
+    X4_C3,
+    X4_C4,
+    X5_C1,
+    X5_C2,
+    X5_C3,
+    X5_C4,
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5
+  ]: Plus5[
+    AsunaTuple4[X1_C1, X1_C2, X1_C3, X1_C4],
+    AsunaTuple4[X2_C1, X2_C2, X2_C3, X2_C4],
+    AsunaTuple4[X3_C1, X3_C2, X3_C3, X3_C4],
+    AsunaTuple4[X4_C1, X4_C2, X4_C3, X4_C4],
+    AsunaTuple4[X5_C1, X5_C2, X5_C3, X5_C4],
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5,
+    AsunaTuple5[Y1, X1_C1, X1_C2, X1_C3, X1_C4],
+    AsunaTuple5[Y2, X2_C1, X2_C2, X2_C3, X2_C4],
+    AsunaTuple5[Y3, X3_C1, X3_C2, X3_C3, X3_C4],
+    AsunaTuple5[Y4, X4_C1, X4_C2, X4_C3, X4_C4],
+    AsunaTuple5[Y5, X5_C1, X5_C2, X5_C3, X5_C4]
+  ] =
+    valPlusWithTypeParameter4.asInstanceOf[Plus5[
+      AsunaTuple4[X1_C1, X1_C2, X1_C3, X1_C4],
+      AsunaTuple4[X2_C1, X2_C2, X2_C3, X2_C4],
+      AsunaTuple4[X3_C1, X3_C2, X3_C3, X3_C4],
+      AsunaTuple4[X4_C1, X4_C2, X4_C3, X4_C4],
+      AsunaTuple4[X5_C1, X5_C2, X5_C3, X5_C4],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple5[Y1, X1_C1, X1_C2, X1_C3, X1_C4],
+      AsunaTuple5[Y2, X2_C1, X2_C2, X2_C3, X2_C4],
+      AsunaTuple5[Y3, X3_C1, X3_C2, X3_C3, X3_C4],
+      AsunaTuple5[Y4, X4_C1, X4_C2, X4_C3, X4_C4],
+      AsunaTuple5[Y5, X5_C1, X5_C2, X5_C3, X5_C4]
+    ]]
+  final def cachePlusWithTypeParameter5[
     X1_C1,
     X1_C2,
     X1_C3,
@@ -374,7 +656,89 @@ object Plus5 {
       override def takeTail5(z: AsunaTuple6[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5]): Y5                                                = z.head
       override def plus5(x: AsunaTuple5[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5], y: Y5): AsunaTuple6[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5] = x.::(y)
     }
-  def plusWithTypeParameter6[
+  final val valPlusWithTypeParameter5: Plus5[
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    AsunaTuple5[Any, Any, Any, Any, Any],
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any]
+  ] = cachePlusWithTypeParameter5
+  final def plusWithTypeParameter5[
+    X1_C1,
+    X1_C2,
+    X1_C3,
+    X1_C4,
+    X1_C5,
+    X2_C1,
+    X2_C2,
+    X2_C3,
+    X2_C4,
+    X2_C5,
+    X3_C1,
+    X3_C2,
+    X3_C3,
+    X3_C4,
+    X3_C5,
+    X4_C1,
+    X4_C2,
+    X4_C3,
+    X4_C4,
+    X4_C5,
+    X5_C1,
+    X5_C2,
+    X5_C3,
+    X5_C4,
+    X5_C5,
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5
+  ]: Plus5[
+    AsunaTuple5[X1_C1, X1_C2, X1_C3, X1_C4, X1_C5],
+    AsunaTuple5[X2_C1, X2_C2, X2_C3, X2_C4, X2_C5],
+    AsunaTuple5[X3_C1, X3_C2, X3_C3, X3_C4, X3_C5],
+    AsunaTuple5[X4_C1, X4_C2, X4_C3, X4_C4, X4_C5],
+    AsunaTuple5[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5],
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5,
+    AsunaTuple6[Y1, X1_C1, X1_C2, X1_C3, X1_C4, X1_C5],
+    AsunaTuple6[Y2, X2_C1, X2_C2, X2_C3, X2_C4, X2_C5],
+    AsunaTuple6[Y3, X3_C1, X3_C2, X3_C3, X3_C4, X3_C5],
+    AsunaTuple6[Y4, X4_C1, X4_C2, X4_C3, X4_C4, X4_C5],
+    AsunaTuple6[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5]
+  ] =
+    valPlusWithTypeParameter5.asInstanceOf[Plus5[
+      AsunaTuple5[X1_C1, X1_C2, X1_C3, X1_C4, X1_C5],
+      AsunaTuple5[X2_C1, X2_C2, X2_C3, X2_C4, X2_C5],
+      AsunaTuple5[X3_C1, X3_C2, X3_C3, X3_C4, X3_C5],
+      AsunaTuple5[X4_C1, X4_C2, X4_C3, X4_C4, X4_C5],
+      AsunaTuple5[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple6[Y1, X1_C1, X1_C2, X1_C3, X1_C4, X1_C5],
+      AsunaTuple6[Y2, X2_C1, X2_C2, X2_C3, X2_C4, X2_C5],
+      AsunaTuple6[Y3, X3_C1, X3_C2, X3_C3, X3_C4, X3_C5],
+      AsunaTuple6[Y4, X4_C1, X4_C2, X4_C3, X4_C4, X4_C5],
+      AsunaTuple6[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5]
+    ]]
+  final def cachePlusWithTypeParameter6[
     X1_C1,
     X1_C2,
     X1_C3,
@@ -460,7 +824,94 @@ object Plus5 {
       override def takeTail5(z: AsunaTuple7[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6]): Y5                                                       = z.head
       override def plus5(x: AsunaTuple6[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6], y: Y5): AsunaTuple7[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6] = x.::(y)
     }
-  def plusWithTypeParameter7[
+  final val valPlusWithTypeParameter6: Plus5[
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    AsunaTuple6[Any, Any, Any, Any, Any, Any],
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any]
+  ] = cachePlusWithTypeParameter6
+  final def plusWithTypeParameter6[
+    X1_C1,
+    X1_C2,
+    X1_C3,
+    X1_C4,
+    X1_C5,
+    X1_C6,
+    X2_C1,
+    X2_C2,
+    X2_C3,
+    X2_C4,
+    X2_C5,
+    X2_C6,
+    X3_C1,
+    X3_C2,
+    X3_C3,
+    X3_C4,
+    X3_C5,
+    X3_C6,
+    X4_C1,
+    X4_C2,
+    X4_C3,
+    X4_C4,
+    X4_C5,
+    X4_C6,
+    X5_C1,
+    X5_C2,
+    X5_C3,
+    X5_C4,
+    X5_C5,
+    X5_C6,
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5
+  ]: Plus5[
+    AsunaTuple6[X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6],
+    AsunaTuple6[X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6],
+    AsunaTuple6[X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6],
+    AsunaTuple6[X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6],
+    AsunaTuple6[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6],
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5,
+    AsunaTuple7[Y1, X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6],
+    AsunaTuple7[Y2, X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6],
+    AsunaTuple7[Y3, X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6],
+    AsunaTuple7[Y4, X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6],
+    AsunaTuple7[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6]
+  ] =
+    valPlusWithTypeParameter6.asInstanceOf[Plus5[
+      AsunaTuple6[X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6],
+      AsunaTuple6[X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6],
+      AsunaTuple6[X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6],
+      AsunaTuple6[X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6],
+      AsunaTuple6[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple7[Y1, X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6],
+      AsunaTuple7[Y2, X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6],
+      AsunaTuple7[Y3, X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6],
+      AsunaTuple7[Y4, X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6],
+      AsunaTuple7[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6]
+    ]]
+  final def cachePlusWithTypeParameter7[
     X1_C1,
     X1_C2,
     X1_C3,
@@ -556,4 +1007,96 @@ object Plus5 {
       override def plus5(x: AsunaTuple7[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6, X5_C7], y: Y5): AsunaTuple8[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6, X5_C7] =
         x.::(y)
     }
+  final val valPlusWithTypeParameter7: Plus5[
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple7[Any, Any, Any, Any, Any, Any, Any],
+    Any,
+    Any,
+    Any,
+    Any,
+    Any,
+    AsunaTuple8[Any, Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple8[Any, Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple8[Any, Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple8[Any, Any, Any, Any, Any, Any, Any, Any],
+    AsunaTuple8[Any, Any, Any, Any, Any, Any, Any, Any]
+  ] = cachePlusWithTypeParameter7
+  final def plusWithTypeParameter7[
+    X1_C1,
+    X1_C2,
+    X1_C3,
+    X1_C4,
+    X1_C5,
+    X1_C6,
+    X1_C7,
+    X2_C1,
+    X2_C2,
+    X2_C3,
+    X2_C4,
+    X2_C5,
+    X2_C6,
+    X2_C7,
+    X3_C1,
+    X3_C2,
+    X3_C3,
+    X3_C4,
+    X3_C5,
+    X3_C6,
+    X3_C7,
+    X4_C1,
+    X4_C2,
+    X4_C3,
+    X4_C4,
+    X4_C5,
+    X4_C6,
+    X4_C7,
+    X5_C1,
+    X5_C2,
+    X5_C3,
+    X5_C4,
+    X5_C5,
+    X5_C6,
+    X5_C7,
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5
+  ]: Plus5[
+    AsunaTuple7[X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6, X1_C7],
+    AsunaTuple7[X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6, X2_C7],
+    AsunaTuple7[X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6, X3_C7],
+    AsunaTuple7[X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6, X4_C7],
+    AsunaTuple7[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6, X5_C7],
+    Y1,
+    Y2,
+    Y3,
+    Y4,
+    Y5,
+    AsunaTuple8[Y1, X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6, X1_C7],
+    AsunaTuple8[Y2, X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6, X2_C7],
+    AsunaTuple8[Y3, X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6, X3_C7],
+    AsunaTuple8[Y4, X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6, X4_C7],
+    AsunaTuple8[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6, X5_C7]
+  ] =
+    valPlusWithTypeParameter7.asInstanceOf[Plus5[
+      AsunaTuple7[X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6, X1_C7],
+      AsunaTuple7[X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6, X2_C7],
+      AsunaTuple7[X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6, X3_C7],
+      AsunaTuple7[X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6, X4_C7],
+      AsunaTuple7[X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6, X5_C7],
+      Y1,
+      Y2,
+      Y3,
+      Y4,
+      Y5,
+      AsunaTuple8[Y1, X1_C1, X1_C2, X1_C3, X1_C4, X1_C5, X1_C6, X1_C7],
+      AsunaTuple8[Y2, X2_C1, X2_C2, X2_C3, X2_C4, X2_C5, X2_C6, X2_C7],
+      AsunaTuple8[Y3, X3_C1, X3_C2, X3_C3, X3_C4, X3_C5, X3_C6, X3_C7],
+      AsunaTuple8[Y4, X4_C1, X4_C2, X4_C3, X4_C4, X4_C5, X4_C6, X4_C7],
+      AsunaTuple8[Y5, X5_C1, X5_C2, X5_C3, X5_C4, X5_C5, X5_C6, X5_C7]
+    ]]
 }
