417e652c83f0a9f6c34dad77a37e4b1402ce56bf
djx314 <djx314@sina.cn>
2019-11-12 20:41:59 +0800

    Little fix.

--- modules/testkit/src/main/scala/asuna/testkit/circe/encoder/JsonObjectAppender.scala
+++ modules/testkit/src/main/scala/asuna/testkit/circe/encoder/JsonObjectAppender.scala
@@ -9,13 +9,18 @@ trait JsonObjectAppender[T, II] extends Any {
 }
 
 object JsonObjectAppender {
-  implicit def asunaCirceEncoder[T](implicit t: ByNameImplicit[Encoder[T]]): Application2[JsonObjectAppender, PropertyTag[T], T, String] = new JsonAppenderApplication(t)
+  implicit def asunaCirceEncoder[T](implicit t: ByNameImplicit[Encoder[T]]): Application2[JsonObjectAppender, PropertyTag[T], T, String] =
+    new support.JsonAppenderApplication(t)
 }
 
-class JsonAppenderApplication[T](private val t: ByNameImplicit[Encoder[T]]) extends AnyVal with Application2[JsonObjectAppender, PropertyTag[T], T, String] {
-  override def application(context: Context2[JsonObjectAppender]): JsonObjectAppender[T, String] = new JsonAppenderImpl(t)
-}
+package support {
 
-class JsonAppenderImpl[T](private val t: ByNameImplicit[Encoder[T]]) extends AnyVal with JsonObjectAppender[T, String] {
-  override def appendField(tt: T, name: String, m: java.util.LinkedHashMap[String, Json]): Unit = m.put(name, t.value(tt))
+  class JsonAppenderApplication[T](private val t: ByNameImplicit[Encoder[T]]) extends AnyVal with Application2[JsonObjectAppender, PropertyTag[T], T, String] {
+    override def application(context: Context2[JsonObjectAppender]): JsonObjectAppender[T, String] = new JsonAppenderImpl(t)
+  }
+
+  class JsonAppenderImpl[T](private val t: ByNameImplicit[Encoder[T]]) extends AnyVal with JsonObjectAppender[T, String] {
+    override def appendField(tt: T, name: String, m: java.util.LinkedHashMap[String, Json]): Unit = m.put(name, t.value(tt))
+  }
+
 }
