bf8b4c65602872a5d93fbf94f7820cff8742f445
djx314 <djx314@sina.cn>
2019-11-12 18:37:14 +0800

    Improve performance.

---
+++ modules/testkit/src/main/scala/asuna/testkit/circe/encoder/EncoderContext.scala
@@ -0,0 +1,19 @@
+package asuna.testkit.circe.encoder
+
+import asuna.{AsunaTuple0, Context2, Plus2}
+import io.circe.Json
+
+object EncoderContext extends Context2[JsonObjectAppender] {
+
+  override def append[X1, X2, Y1, Y2, Z1, Z2](x: JsonObjectAppender[X1, X2], y: JsonObjectAppender[Y1, Y2])(
+    p: Plus2[X1, X2, Y1, Y2, Z1, Z2]
+  ): JsonObjectAppender[Z1, Z2] = { (obj, name, m) =>
+    y.appendField(p.takeTail1(obj), p.takeTail2(name), m)
+    x.appendField(p.takeHead1(obj), p.takeHead2(name), m)
+  }
+
+  override val start: JsonObjectAppender[AsunaTuple0, AsunaTuple0] = { (_: AsunaTuple0, _: AsunaTuple0, _: java.util.LinkedHashMap[String, Json]) =>
+    ()
+  }
+
+}
