a6ac442e480baeddf3290b3353ea7c9ff70fcdcd
djx314 <djx314@sina.cn>
2019-11-12 11:56:49 +0800

    Bump version.

--- project/AsunaSettings.scala
+++ project/AsunaSettings.scala
@@ -9,7 +9,7 @@ object AsunaSettings {
   val circeVersion        = "0.12.2"
   val scalaTestVersion    = "3.1.0-RC3"
 
-  val setting1 = scalaVersion := currentScalaVersion
+  val setting1 = scalaVersion := scala_2_12_Version
   val setting2 = crossScalaVersions := Seq(scala_2_12_Version, currentScalaVersion)
   val setting3 = scalacOptions ++= Seq("-feature", "-deprecation", "-encoding", "utf-8" /*"-language:Scala2", "-Ywarn-unused-import", "-Xlog-implicits"*/ )
   val setting4 = org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile := false
@@ -32,6 +32,7 @@ object AsunaSettings {
   val vSetting3             = bintrayOrganization := Some("scalax")
   val vSetting4             = bintrayRepository := "asuna"
   val vSetting5             = licenses += ("MIT", url("http://opensource.org/licenses/MIT"))
-  val projectVersionSetting = Seq(vSetting1, vSetting2, vSetting3, vSetting4, vSetting5)
+  val vSetting6             = bintrayVcsUrl := Some("https://github.com/scalax/asuna.git")
+  val projectVersionSetting = Seq(vSetting1, vSetting2, vSetting3, vSetting4, vSetting5, vSetting6)
 
 }
