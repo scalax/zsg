2bf4ac2e48c67a1a5ae263464ff58fea86666f95
djx314 <djx314@sina.cn>
2019-11-22 00:05:55 +0800

    Use inline in BuildTag.

--- modules/core/src/test/scala/TagTest.scala
+++ modules/core/src/test/scala/TagTest.scala
@@ -8,14 +8,14 @@ object TagTest {
   class Test2
   class Test3
 
-  val tag1 = BuildTag.nodeTag(
+  val tag1 = BuildTag.lift(BuildTag.nodeTag(
     BuildTag.tag(new AppendTag[String], new AppendTag[Int], new AppendTag[Long], new AppendTag[String]),
     BuildTag.tag(new AppendTag[Long], new AppendTag[Int], new AppendTag[String])
-  )
-  val tag2 = BuildTag.nodeTag(
+  ))
+  val tag2 = BuildTag.lift(BuildTag.nodeTag(
     BuildTag.tag(new AppendTag[Test1], new AppendTag[Int], new AppendTag[Long], new AppendTag[String]),
     BuildTag.tag(new AppendTag[Long], new AppendTag[Test2], new AppendTag[Test3])
-  )
+  ))
 
   val result = BuildTag.mergeTag(tag1, tag2)
   val ii: AppendTag[NodeTag2[
