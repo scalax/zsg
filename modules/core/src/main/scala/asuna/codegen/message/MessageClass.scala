a9ca9641db4aba346f0d29c8abaac203ef33878a
djx314 <djx314@sina.cn>
2019-12-09 15:04:44 +0800

    Change to import.

--- modules/core/src/main/scala/asuna/codegen/message/MessageClass.scala
+++ modules/core/src/main/scala/asuna/codegen/message/MessageClass.scala
@@ -1,4 +1,5 @@
 package asuna.debug
+import scala.annotation.implicitNotFound
 class `The implicit not founded property's index is 1. Please change findX(context1) to .findX.find1(context1) to continue debug.`
 class `The implicit not founded property's index is 2. Please change findX(context1) to .findX.find2(context1) to continue debug.`
 class `The implicit not founded property's index is 3. Please change findX(context1) to .findX.find3(context1) to continue debug.`
@@ -7,7 +8,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context1) to .findX.find6(context1) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context1) to .findX.find7(context1) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context1) to .findX.find8(context1) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication1[Message, F[_], Tag, X1] extends asuna.Application1[F, Tag, X1] {
   override def application(context: asuna.Context1[F]): F[X1]
 }
@@ -26,7 +27,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context2) to .findX.find6(context2) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context2) to .findX.find7(context2) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context2) to .findX.find8(context2) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication2[Message, F[_, _], Tag, X1, X2] extends asuna.Application2[F, Tag, X1, X2] {
   override def application(context: asuna.Context2[F]): F[X1, X2]
 }
@@ -47,7 +48,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context3) to .findX.find6(context3) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context3) to .findX.find7(context3) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context3) to .findX.find8(context3) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication3[Message, F[_, _, _], Tag, X1, X2, X3] extends asuna.Application3[F, Tag, X1, X2, X3] {
   override def application(context: asuna.Context3[F]): F[X1, X2, X3]
 }
@@ -68,7 +69,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context4) to .findX.find6(context4) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context4) to .findX.find7(context4) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context4) to .findX.find8(context4) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication4[Message, F[_, _, _, _], Tag, X1, X2, X3, X4] extends asuna.Application4[F, Tag, X1, X2, X3, X4] {
   override def application(context: asuna.Context4[F]): F[X1, X2, X3, X4]
 }
@@ -89,7 +90,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context5) to .findX.find6(context5) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context5) to .findX.find7(context5) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context5) to .findX.find8(context5) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication5[Message, F[_, _, _, _, _], Tag, X1, X2, X3, X4, X5] extends asuna.Application5[F, Tag, X1, X2, X3, X4, X5] {
   override def application(context: asuna.Context5[F]): F[X1, X2, X3, X4, X5]
 }
@@ -110,7 +111,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context6) to .findX.find6(context6) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context6) to .findX.find7(context6) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context6) to .findX.find8(context6) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication6[Message, F[_, _, _, _, _, _], Tag, X1, X2, X3, X4, X5, X6] extends asuna.Application6[F, Tag, X1, X2, X3, X4, X5, X6] {
   override def application(context: asuna.Context6[F]): F[X1, X2, X3, X4, X5, X6]
 }
@@ -131,7 +132,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context7) to .findX.find6(context7) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context7) to .findX.find7(context7) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context7) to .findX.find8(context7) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication7[Message, F[_, _, _, _, _, _, _], Tag, X1, X2, X3, X4, X5, X6, X7] extends asuna.Application7[F, Tag, X1, X2, X3, X4, X5, X6, X7] {
   override def application(context: asuna.Context7[F]): F[X1, X2, X3, X4, X5, X6, X7]
 }
@@ -152,7 +153,7 @@ class `The implicit not founded property's index is 5. Please change findX(conte
 class `The implicit not founded property's index is 6. Please change findX(context8) to .findX.find6(context8) to continue debug.`
 class `The implicit not founded property's index is 7. Please change findX(context8) to .findX.find7(context8) to continue debug.`
 class `The implicit not founded property's index is 8. Please change findX(context8) to .findX.find8(context8) to continue debug.`
-@scala.annotation.implicitNotFound(msg = "${Message}")
+@implicitNotFound(msg = "${Message}")
 trait NodeDebugApplication8[Message, F[_, _, _, _, _, _, _, _], Tag, X1, X2, X3, X4, X5, X6, X7, X8] extends asuna.Application8[F, Tag, X1, X2, X3, X4, X5, X6, X7, X8] {
   override def application(context: asuna.Context8[F]): F[X1, X2, X3, X4, X5, X6, X7, X8]
 }
