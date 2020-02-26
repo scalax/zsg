34c33700bc81a45ef166f08db32749759b4ac62a
djx314 <djx314@sina.cn>
2020-02-26 19:35:32 +0800

    Use proxy.

--- project/AsunaSettings.scala
+++ project/AsunaSettings.scala
@@ -8,7 +8,7 @@ object AsunaSettings {
   val scala212Version     = "2.12.10"
   val dottyVersion        = "0.22.0-RC1"
   val circeVersion        = "0.12.2"
-  val scalaTestVersion    = "3.1.0-RC3"
+  val scalaTestVersion    = "3.1.0"
 
   val setting1 = scalaVersion := currentScalaVersion
   val setting2 = crossScalaVersions := Seq(scala212Version, currentScalaVersion)
@@ -17,7 +17,7 @@ object AsunaSettings {
   val setting5 = transitiveClassifiers := Seq("sources")
   val setting6 = crossScalaVersions := Seq(dottyVersion, scala212Version, currentScalaVersion)
 
-  val scalaVersionSettings = Seq(setting1, setting2)
+  val scalaVersionSettings = Seq(setting1, setting2 )
   val commonSettings       = Seq(setting3, setting4, setting5)
 
   val dottyVersionSettings = Seq(setting1, setting6)
@@ -32,7 +32,7 @@ object AsunaSettings {
 
   val upickleDependencies = Seq("com.lihaoyi" %% "upickle" % "0.8.0")
 
-  val vSetting1             = version := "0.0.3-20200222SNAP1"
+  val vSetting1             = version := "0.0.3-20200226SNAP2"
   val vSetting2             = organization := "org.scalax"
   val vSetting3             = bintrayOrganization := Some("scalax")
   val vSetting4             = bintrayRepository := "asuna"
