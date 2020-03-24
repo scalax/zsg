545337312fe6090a58773b5dee5ab51bbb7a72ed
djx314 <djx314@sina.cn>
2020-03-24 12:01:38 +0800

    Add hunhe.

--- examples/src/main/scala/asuna/sample04_round/Round.scala
+++ examples/src/main/scala/asuna/sample04_round/Round.scala
@@ -5,10 +5,11 @@ object Round {
   trait 金 {
     type T
     def tuple_1: 火[T]
-    def tuple_2: 火星[T] = 火星(birthday = tuple_1.birthday)
+    def tuple_2: 火星[T]   = 火星(birthday = tuple_1.birthday)
+    def propertyName: 混合 = 混合(id = tuple_1.id, name = tuple_1.name)
   }
 
-  type 水 = (Long, String, Int)
+  type 水 = (混合, Int)
 
   trait 木 {
     def function[T](i: 火星[T]): 火[T]
@@ -16,18 +17,19 @@ object Round {
 
   case class 火[T](id: Long, name: String, age: Int, birthday: T)
   case class 火星[T](birthday: T)
+  case class 混合(id: Long, name: String)
 
-  type 土[T] = (Long, String, Int, T) //一方水土养一方人，只有水和土是数据之源
+  type 土[T] = (混合, Int, T) //一方水土养一方人，只有水和土是数据之源
 
-  def 金生水(i: 金): 水 = (i.tuple_1.id, i.tuple_1.name, i.tuple_1.age)
+  def 金生水(i: 金): 水 = (混合(i.tuple_1.id, i.tuple_1.name), i.tuple_1.age)
   def 水生木(i: 水): 木 = new 木 {
-    override def function[E](ii: 火星[E]): 火[E] = 火(id = i._1, name = i._2, age = i._3, birthday = ii.birthday)
+    override def function[E](ii: 火星[E]): 火[E] = 火(id = i._1.id, name = i._1.name, age = i._2, birthday = ii.birthday)
   }
   def 木生火[E](i: 火星[E], ii: 木): 火[E] = ii.function(i) //火星将会汇入木生火的火中
-  def 火生土[E](i: 火[E]): 土[E]         = (i.id, i.name, i.age, i.birthday)
+  def 火生土[E](i: 火[E]): 土[E]         = (混合(i.id, i.name), i.age, i.birthday)
   def 土生金[E](i: 土[E]): 金 = new 金 {
     override type T = E
-    override def tuple_1: 火[T] = 火(id = i._1, name = i._2, age = i._3, birthday = i._4)
+    override def tuple_1: 火[T] = 火(id = i._1.id, name = i._1.name, age = i._2, birthday = i._3)
   }
 
 }
