c528698e62266b43909f133d747b288adaa2ea3e
djx314 <djx314@sina.cn>
2020-03-27 21:53:57 +0800

    Add round2.

--- examples/src/main/scala/asuna/sample04_round/Round2.scala
+++ examples/src/main/scala/asuna/sample04_round/Round2.scala
@@ -1,31 +1,25 @@
 package asuna.sample04_round
 
-object Round {
+object Round2 {
 
-  trait 金 {
+  trait I1 {
     type T
-    def tuple_1: 火[T]
-    def tuple_2: 火星[T]   = 火星(birthday = tuple_1.birthday)
-    def propertyName: 混合 = {
-      val ii = tuple_1
-      混合(id = ii.id, name = ii.name)
-    }
+    def tuple_1: I4[T]
+    def tuple_2: I4Bit[T] = I4Bit(birthday = tuple_1.birthday)
   }
 
-  type 水 = (混合, Int)
+  type I2 = (Long, String, Int)
 
-  trait 木 {
-    def function[T](i: 火星[T]): 火[T]
-    def propertyName: 混合
+  trait I3 {
+    def function[T](i: I4Bit[T]): I4[T]
   }
 
-  case class 火[T](id: Long, name: String, age: Int, birthday: T)
-  case class 火星[T](birthday: T)
-  case class 混合(id: Long, name: String)
+  case class I4[T](id: Long, name: String, age: Int, birthday: T)
+  case class I4Bit[T](birthday: T)
 
-  type 土[T] = (混合, Int, T) //一方水土养一方人，只有水和土是数据之源
+  type I5[T] = (Long, String, Int, T)
 
-  def 金生水(i: 金): 水 = (混合(i.tuple_1.id, i.tuple_1.name), i.tuple_1.age)
+  /*def 金生水(i: 金): 水 = (混合(i.tuple_1.id, i.tuple_1.name), i.tuple_1.age)
   def 水生木(i: 水): 木 = new 木 {
     override def function[E](ii: 火星[E]): 火[E] = {
       val iii = propertyName
@@ -38,6 +32,6 @@ object Round {
   def 土生金[E](i: 土[E]): 金 = new 金 {
     override type T = E
     override def tuple_1: 火[T] = 火(id = i._1.id, name = i._1.name, age = i._2, birthday = i._3)
-  }
+  }*/
 
 }
