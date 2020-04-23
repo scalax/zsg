04b34d812ef4cdb93bc7a1a097031bc4c336c9c6
djx314 <djx314@sina.cn>
2020-04-23 16:01:04 +0800

    New origin.

--- project/AsunaSettings.scala
+++ project/AsunaSettings.scala
@@ -4,10 +4,10 @@ import bintray.BintrayKeys._
 
 object AsunaSettings {
 
-  val currentScalaVersion = "2.13.1"
+  val currentScalaVersion = "2.13.2"
   val scala212Version     = "2.12.11"
   val scala211Version     = "2.11.12"
-  val dottyVersion        = "0.22.0-RC1"
+  val dottyVersion        = "0.23.0-RC1"
 
   val setting1 = scalaVersion := currentScalaVersion
   val setting2 = crossScalaVersions := Seq(scala211Version, scala212Version, currentScalaVersion)
