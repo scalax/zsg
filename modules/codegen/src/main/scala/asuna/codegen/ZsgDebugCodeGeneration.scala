1b1b7f07571e8706f322cd2f0e8717584252aba6
djx314 <djx314@sina.cn>
2020-05-24 23:38:59 +0800

    Debug instance.

--- modules/codegen/src/main/scala/asuna/codegen/ZsgDebugCodeGeneration.scala
+++ modules/codegen/src/main/scala/asuna/codegen/ZsgDebugCodeGeneration.scala
@@ -7,34 +7,29 @@ import scala.io.Source
 
 object ZsgDebugCodeGeneration {
 
-  val root2XDir =
-    Paths.get(".").resolve("modules").resolve("debug").resolve("src").resolve("main").resolve("scala-2").resolve("zsg").resolve("codegen")
+  val root2XDir = Paths.get(".").resolve("modules").resolve("debug").resolve("src").resolve("main").resolve("scala-2").resolve("zsg").resolve("codegen")
 
-  val rootAllDir =
-    Paths.get(".").resolve("modules").resolve("debug").resolve("src").resolve("main").resolve("scala").resolve("zsg").resolve("codegen")
+  val rootAllDir = Paths.get(".").resolve("modules").resolve("debug").resolve("src").resolve("main").resolve("scala").resolve("zsg").resolve("codegen")
 
-  val rootDottyDir =
-    Paths.get(".").resolve("modules").resolve("debug").resolve("src").resolve("main").resolve("scala-3").resolve("zsg").resolve("codegen")
+  val rootDottyDir = Paths.get(".").resolve("modules").resolve("debug").resolve("src").resolve("main").resolve("scala-3").resolve("zsg").resolve("codegen")
 
   def main(i: Array[String]): Unit = {
 
     {
-      for (proNum <- 1 to 127) yield {
+      for (proNum <- 1 to 64) yield {
         val filePath = root2XDir.resolve("debug").resolve(s"DebugCaseClass${proNum}.scala")
         Files.createDirectories(filePath.getParent)
-        val writer = new PrintWriter(filePath.toFile, "utf-8")
-        val content =
-          StringUtil.trimLines(zsg.codegen.debug.txt.DebugCaseClass(maxPropertyNum = ZsgParameters.maxPropertyNum)(proNum = proNum)(isDotty = false).body)
+        val writer  = new PrintWriter(filePath.toFile, "utf-8")
+        val content = StringUtil.trimLines(zsg.codegen.debug.txt.DebugCaseClass(maxPropertyNum = ZsgParameters.maxPropertyNum)(proNum = proNum)(isDotty = false).body)
         writer.println(content)
         writer.close()
       }
 
-      for (proNum <- 1 to 127) yield {
+      for (proNum <- 1 to 64) yield {
         val filePath = rootDottyDir.resolve("debug").resolve(s"DebugCaseClass${proNum}.scala")
         Files.createDirectories(filePath.getParent)
-        val writer = new PrintWriter(filePath.toFile, "utf-8")
-        val content =
-          StringUtil.trimLines(zsg.codegen.debug.txt.DebugCaseClass(maxPropertyNum = ZsgParameters.maxPropertyNum)(proNum = proNum)(isDotty = true).body)
+        val writer  = new PrintWriter(filePath.toFile, "utf-8")
+        val content = StringUtil.trimLines(zsg.codegen.debug.txt.DebugCaseClass(maxPropertyNum = ZsgParameters.maxPropertyNum)(proNum = proNum)(isDotty = true).body)
         writer.println(content)
         writer.close()
       }
@@ -43,14 +38,13 @@ object ZsgDebugCodeGeneration {
     {
       val filePath = rootAllDir.resolve("debug").resolve(s"ZsgTupleToDebugCaseClass.scala")
       Files.createDirectories(filePath.getParent)
-      val writer = new PrintWriter(filePath.toFile, "utf-8")
-      val content =
-        StringUtil.trimLines(zsg.codegen.debug.txt.ZsgTupleToDebugCaseClass().body)
+      val writer  = new PrintWriter(filePath.toFile, "utf-8")
+      val content = StringUtil.trimLines(zsg.codegen.debug.txt.ZsgTupleToDebugCaseClass().body)
       writer.println(content)
       writer.close()
     }
 
-    {
+    /*{
       {
         val filePath = root2XDir.resolve("debug").resolve(s"DebugCaseClassTupleTag64.scala")
         Files.createDirectories(filePath.getParent)
@@ -69,7 +63,7 @@ object ZsgDebugCodeGeneration {
         writer.println(content)
         writer.close()
       }
-    }
+    }*/
 
   }
 
