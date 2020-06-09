dd5bd172e7205faeb50bca3fd74e419e3d299e9b
djx314 <djx314@sina.cn>
2020-06-09 17:04:22 +0800

    Benchmark fix.

---
+++ modules/benchmark/src/main/scala/zsg/json/encoder/benchmark/JsonEncoderCompareBenchmark.scala
@@ -0,0 +1,51 @@
+package zsg.json.encoder.benchmark
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+import upickle.default.{ReadWriter => RW}
+import zsg.testkit.circe.ACirce
+import io.circe.Encoder
+import zsg.json.model.{Bar, Foo, Model1}
+
+@BenchmarkMode(Array(Mode.Throughput)) // 测试方法平均执行时间
+@OutputTimeUnit(TimeUnit.SECONDS)      // 输出结果的时间粒度为微秒
+@State(Scope.Thread)                   // 每个测试线程一个实例
+class JsonEncoderCompareBenchmark {
+
+  object uPickleRW {
+    import upickle.default.macroRW
+    implicit val barRW: RW[Bar] = macroRW
+    implicit val fooRW: RW[Foo] = macroRW
+  }
+
+  object rawCirceEncoder {
+    import io.circe.generic.semiauto._
+    implicit val barEncoder: Encoder.AsObject[Bar] = deriveEncoder
+    implicit val fooEncoder: Encoder.AsObject[Foo] = deriveEncoder
+  }
+
+  object zsgEncoder {
+    implicit val barEncoder: Encoder.AsObject[Bar] = ACirce.encodeCaseClass
+    implicit val fooEncoder: Encoder.AsObject[Foo] = ACirce.encodeCaseClass
+  }
+
+  val model: Bar = Model1.bar
+
+  @Benchmark
+  def uPickleEncode: String = {
+    import upickle.default._
+    write(model)(uPickleRW.barRW)
+  }
+
+  @Benchmark
+  def rawCirceEncode: String = {
+    rawCirceEncoder.barEncoder(model).noSpaces
+  }
+
+  @Benchmark
+  def zsgCirceEncode: String = {
+    zsgEncoder.barEncoder(model).noSpaces
+  }
+
+}
---
+++ modules/benchmark/src/main/scala/zsg/json/model/Foo.scala
@@ -0,0 +1,16 @@
+package zsg.json.model
+
+case class Foo(
+  i1: Int,
+  i2: String,
+  i3: String,
+  i4: String,
+  i5: Int,
+  i6: String,
+  i7: String,
+  i8: Long,
+  i9: String,
+  i10: Long,
+  i11: String,
+  i12: String
+)
